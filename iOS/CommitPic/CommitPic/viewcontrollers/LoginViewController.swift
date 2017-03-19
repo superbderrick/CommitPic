37b1d402a1832406f8dc2932195348417caf19dd
superbDerrick <kang.derrick@gmail.com>
2017-03-19 10:55:15 +0900

    added loginController

---
+++ iOS/CommitPic/CommitPic/viewcontrollers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  CommitPic
+//
+//  Created by Kang Jinyeoung on 2017. 3. 19..
+//  Copyright © 2017년 Superbderrick. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
