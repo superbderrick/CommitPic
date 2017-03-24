a5e5a798b992e1f91899e1881bb2376e9d8f2b6c
derrick <derrick.kang@nexstreaming.com>
2017-03-24 09:12:42 +0900

    added temptableview to test daily statics api with table cell ,It is going to change file name after test

---
+++ iOS/CommitPic/CommitPic/ActivityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTableViewCell.swift
+//  CommitPic
+//
+//  Created by derrick on 24/03/2017.
+//  Copyright © 2017 Superbderrick. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
